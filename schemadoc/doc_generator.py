5e6a0821bfba6f18e43b61574cb6932f225a85f4
Maksym Markov <maksym.markov@gmail.com>
2014-10-25 15:00:09 -0400

    switched form usign separate dictionary to direct using schema objects

--- schemadoc/doc_generator.py
+++ schemadoc/doc_generator.py
@@ -1,5 +1,4 @@
 import os
-from graphviz import Digraph
 from jinja2 import Environment, PackageLoader
 
 
@@ -17,30 +16,39 @@ class DocGenerator(object):
         if not os.path.exists(self._tables_folder):
             os.makedirs(self._tables_folder)
         # generate pages for each table
-        for table in self._db.tables:
-            table_url = self._generate_table_page(table)
-            table_pages.append({'table': table, 'url': table_url})
+        for table_name in self._db.tables:
+            table = self._db.tables[table_name]
+            # store table page url for future references
+            table.page_url = self._generate_table_page(table)
+
         # generate index page
-        self._generate_home_page(table_pages)
+        self._generate_home_page()
 
-    def _render_table_page(self, table_name):
+    def _render_table_page(self, table):
         template = self._env.get_template('table_page.html')
-        return template.render(table_name=table_name)
+        return template.render(table=table)
 
-    def _generate_table_page(self, table_name):
-        table_page = self._render_table_page(table_name)
+    def _generate_table_page(self, table):
+
+        """
+        Method which creates documentation page per table
+        :param table: TableSpecification object form schema objects
+        :return: relative url for newly generate table page
+        """
+        table_name = table.name
+        table_page = self._render_table_page(table)
         table_page_path = os.path.join(self._tables_folder, "%s.html"%table_name)
         with open(table_page_path, "wt") as f:
             f.write(table_page)
         url = os.path.relpath(table_page_path, self._folder)
         return url
 
-    def _render_home_page(self, table_pages):
+    def _render_home_page(self):
         template = self._env.get_template('home_page.html')
-        return template.render(table_pages=table_pages)
+        return template.render(db=self._db)
 
-    def _generate_home_page(self, table_pages):
-        home_page = self._render_home_page(table_pages)
+    def _generate_home_page(self):
+        home_page = self._render_home_page()
         home_page_path = os.path.join(self._folder, 'index.html')
         with open(home_page_path, "wt") as f:
             f.write(home_page)
